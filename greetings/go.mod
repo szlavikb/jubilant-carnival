995342f2794b8068aa33f91ec7ca195b8db11e03
FatLlama <77354514+szlavikb@users.noreply.github.com>
2025-10-26 16:14:17 +0100

    add greetings module and implement Hello function for personalized messages (#1)
    Co-authored-by: barnabas <barnabas.szlavik@barre.hu>

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module github.com/szlavikb/jubilant-carnival/greetings
+
+go 1.25.0
--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"rsc.io/quote"
+	"github.com/szlavikb/jubilant-carnival/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("Alice")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
 }
